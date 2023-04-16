902a16fbeb19bc953d8de77445d351c606910a52
Taylor Wilmot <tgwilmot@wisc.edu>
2023-04-16 12:40:36 -0500

    BT2H-9 Mag Reed Changes
    DESCRIPTION: Removed the code for an LED
    
    TESTS: none required

--- Hardware/magReed/magReed.ino
+++ Hardware/magReed/magReed.ino
@@ -1,10 +1,8 @@
 const int REED_PIN = 2;	// Pin connected to reed switch
-const int LED_PIN = 13;	// LED pin
 
 void setup() {
 	Serial.begin(9600);
 	pinMode(REED_PIN, INPUT_PULLUP);	// Enable internal pull-up for the reed switch
-	pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
@@ -13,10 +11,8 @@ void loop() {
 	// If the pin reads low, the switch is closed.
 	if (proximity == LOW) {
 		Serial.println("Switch closed");
-		digitalWrite(LED_PIN, HIGH);	// Turn the LED on
 	}
 	else {
 		Serial.println("Switch opened");
-		digitalWrite(LED_PIN, LOW);		// Turn the LED off
 	}
 }
