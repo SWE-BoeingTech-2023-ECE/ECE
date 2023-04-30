f0a532dc399642991b4afdd5dca0ab378762e598
riyabhat2002 <72653022+riyabhat2002@users.noreply.github.com>
2023-04-29 21:35:43 -0500

    Added the Google sheets code - Compiles but not sure if it works yet

--- Hardware/Wifi_Connect/Wifi_Connect.ino
+++ Hardware/Wifi_Connect/Wifi_Connect.ino
@@ -1,35 +1,139 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <LiquidCrystal.h>
 #include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include "HTTPSRedirect.h"
 
-// Set WiFi credentials
-#define WIFI_SSID "iPhone"
-#define WIFI_PASS "thalia2002"
+// Data wire is connected to the Arduino digital pin 2
+#define ONE_WIRE_BUS 2
+
+// Setup a oneWire instance to communicate with any OneWire devices
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature sensor 
+DallasTemperature sensors(&oneWire);
+
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(4, 6, 10, 11, 12, 13);
+
+float temp = 0;
+float tempF = 0;
+
+// WiFi credentials
+const char* ssid = "iPhone";
+const char* password = "thalia2002";
+
+// Enter Google Script Deployment ID:
+const char *GScriptId = "AKfycbwKKOYwCZPJYxJt2twsKtP--mBwbc0nb5ZL4s55_XvVWyzKRNazkkMvif8cwJ3NS6vJHA";
+
+// Enter command (insert_row or append_row) and your Google Sheets sheet name (default is Sheet1):
+String payload_base =  "{\"command\": \"insert_row\", \"sheet_name\": \"Sheet1\", \"values\": ";
+String payload = "";
+
+// Google Sheets setup (do not edit)
+const char* host = "script.google.com";
+const int httpsPort = 443;
+const char* fingerprint = "";
+String url = String("/macros/s/") + GScriptId + "/exec";
+HTTPSRedirect* client = nullptr;
 
 void setup() {
-  // Setup serial port
+  // Start the serial communication
   Serial.begin(115200);
-  Serial.println();
 
-  // Begin WiFi
-  WiFi.begin(WIFI_SSID, WIFI_PASS);
-
-  // Connecting to WiFi...
-  Serial.print("Connecting to ");
-  Serial.print(WIFI_SSID);
-  // Loop continuously while WiFi is not connected
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(100);
-    Serial.println("Connecting...");
+  // Connect to WiFi
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print("Connecting..");
   }
-
-  // Connected to WiFi
-  Serial.println();
   Serial.print("Connected! IP address: ");
   Serial.println(WiFi.localIP());
 
+  // Start the LCD
+  lcd.begin(16, 2);
+
+  // Start the DS18B20 sensor
+  sensors.begin();
+
+  // Use HTTPSRedirect class to create a new TLS connection
+  client = new HTTPSRedirect(httpsPort);
+  client->setInsecure();
+  client->setPrintResponseBody(true);
+  client->setContentTypeHeader("application/json");
+  
+  Serial.print("Connecting to ");
+  Serial.println(host);
+
+  // Try to connect for a maximum of 5 times
+    bool flag = false;
+  for (int i=0; i<5; i++){ 
+    int retval = client->connect(host, httpsPort);
+    if (retval == 1){
+       flag = true;
+       Serial.println("Connected");
+       break;
+    }
+    else
+      Serial.println("Connection failed. Retrying...");
+  }
+  if (!flag){
+    Serial.print("Could not connect to server: ");
+    Serial.println(host);
+    return;
+  }
+  delete client;    // delete HTTPSRedirect object
+  client = nullptr; // delete HTTPSRedirect object
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Wait for the temperature to be ready
+  sensors.requestTemperatures();
 
+  // Read the temperature in Celsius and Fahrenheit
+  float celsius = sensors.getTempCByIndex(0);
+  float farenheit = (celsius * 1.8) + 32.0;
+
+  lcd.setCursor(1,0);  // set the cursor to column 15, line 0
+  lcd.print(farenheit);
+  lcd.print((char)223);
+  lcd.print("F");
+  delay(2000);
+  lcd.clear();
+
+  static bool flag = false;
+  if (!flag){
+    client = new HTTPSRedirect(httpsPort);
+    client->setInsecure();
+    flag = true;
+    client->setPrintResponseBody(true);
+    client->setContentTypeHeader("application/json");
+  }
+  if (client != nullptr){
+    if (!client->connected()){
+      client->connect(host, httpsPort);
+    }
+  }
+  else{
+    Serial.println("Error creating client object!");
+  }
+
+  // Create json object string to send to Google Sheets
+  payload = payload_base + "\"" + celsius + "," + farenheit + "\"}";
+  
+  // Publish data to Google Sheets
+  Serial.println("Publishing data...");
+  Serial.println(payload);
+  if(client->POST(url, host, payload)){ 
+    // do stuff here if publish was successful
+  }
+  else{
+    // do stuff here if publish was not successful
+    Serial.println("Error while connecting");
+  }
+
+  // a delay of several seconds is required before publishing again    
+  delay(5000);
 }
